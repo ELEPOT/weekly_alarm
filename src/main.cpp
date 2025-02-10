6e4f6f44759df1289fe6bcf9ec9bb6aa5297b02e
ELEPOT <elepotmail0@gmail.com>
2025-02-10 16:44:53 +0800

    TESTED: seg-7

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,34 @@
 #include <Arduino.h>
-#include <DFPlayerMini_Fast.h>
+#include <LedControl.h>
 
-#if !defined(UBRR1H)
-#include <SoftwareSerial.h>
-SoftwareSerial dfSerial(2, 3);
-#endif
+auto seg7 = LedControl(12, 11, 10, 1);
 
-DFPlayerMini_Fast df;
+static const byte digitMap[] = {
+    0b01111110,0b00110000,0b01101101,0b01111001,0b00110011,0b01011011,0b01011111,0b01110010,0b01111111,0b01111011
+};
 
-void setup() {
-    Serial.begin(115200);
-
-#if !defined(UBRR1H)
-    dfSerial.begin(9600);
-    df.begin(dfSerial, true);
-#else
-    Serial1.begin(9600)
-    df.begin(Serial1, true);
-#endif
-
-    df.volume(2);
-    delay(1000);
-    df.repeatFolder(1);
+void setSeg7Digit(const int index, const int value) {
+    // setCol for common anode 7-segments
+    seg7.setRow(0, index, digitMap[value]);
 }
 
-void loop() {
+void setup() {
+    seg7.shutdown(0, false);
+    seg7.setIntensity(0, 8);
+    seg7.clearDisplay(0);
 
+    setSeg7Digit(0, 1);
+    // setSeg7Digit(1, 0);
+}
+
+int i = 0;
+
+void loop() {
+    // setSeg7Digit(0, i / 10);
+    // setSeg7Digit(1, i % 10);
+    //
+    // i ++;
+    // i = i % 100;
+    //
+    // delay(500);
 }
